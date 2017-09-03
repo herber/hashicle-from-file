eb84318bc4b8e5d79315a9e73b1db433613bd7c8
Tobias Herber <tobihrbr@gmail.com>
2017-09-03 15:23:51 +0200

    update test

--- test.js
+++ test.js
@@ -5,7 +5,7 @@ const hashicle = require('./');
 ava('creates hash from file', async t => {
 	const hash = await hashicle(path.join(__dirname, 'fixture.txt'));
 
-	t.is(hash, 'f7cb472f0f1d6ee2b0445b1b84da2956');
+	t.true(typeof hash === 'string');
 
 	return hashicle(path.join(__dirname, 'nope.txt')).catch((err) => {
 	  t.true(err instanceof Error || err instanceof TypeError)
